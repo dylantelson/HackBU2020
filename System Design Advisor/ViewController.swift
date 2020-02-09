5731880ab29c5c89ae9f8eaa374dd1b502f50fcd
dylantelson <dylantelson@gmail.com>
2020-02-08 22:54:26 -0500

    Changed the whole format. Now the questions come in groups, and slide up to give space for the new ones. Also added new questions, and made it so the storage space question has a 3 component picker.

--- System Design Advisor/ViewController.swift
+++ System Design Advisor/ViewController.swift
@@ -10,70 +10,167 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
     
+    //These are the parent views to move
+    @IBOutlet weak var computerPartQuestion: UIView!
+    @IBOutlet weak var budgetQuestion: UIView!
+    @IBOutlet weak var upgradeQuestion: UIView!
+    @IBOutlet weak var placeholderQuestion: UIView!
     
-    @IBOutlet var TextField1: UITextField!
-    @IBOutlet var TextField2: UITextField!
+    @IBOutlet weak var cloudSupportSwitch: UISwitch!
+    
+    @IBOutlet weak var upgradeSwitch: UISwitch!
+    
+    @IBOutlet var computerPartTextField: UITextField!
+    @IBOutlet var budgetTextField: UITextField!
     @IBOutlet var TextField3: UITextField!
+    @IBOutlet weak var selectedPartTextField: UITextField!
+    
     var textFields = [UITextField]()
+    var hiddenViews = [UIView]()
+    
+    @IBOutlet weak var selectedPartText: UILabel!
+    //default computer part is CPU Processor
+    var computerPart = "CPU Processor"
     
     @IBOutlet weak var pickerView: UIPickerView!
     
     @IBOutlet weak var CloudSupport: UIView!
     
-    var picker1Type = ["CPU Processors", "Motherboards", "Power Supplies", "Storage"]
+    var computerPartList = ["CPU Processor", "Motherboard", "Power Supply", "Storage"]
+    var processorTypes = ["Optional", "Intel i3", "Intel i5", "Intel i7"]
+    var storageBrands = ["Seagate", "Toshiba", "Samsung"]
+    var seagateTypes = ["Barracuda", "Ironwolf"]
+    var toshibaTypes = ["X300", "XG5"]
+    var samsungTypes = ["EVO", "T5", "970 EVO Plus"]
+    var storageSize = ["256GB", "512GB", "1TB", "2TB", "5TB"]
     var picker2Type = ["2test1", "2test2", "2test3"]
     
+    var cloudSupportOn = true
+    
+    var listOfBrandTypes : [[String]] = [[]]
+    
+    var currentStorageBrand = "Seagate"
+    
     var currentTextField = UITextField()
+    var currentQuestion = UIView()
+    
+    var screenSize = UIScreen.main.bounds
     
     @IBOutlet weak var errorAlert: UIView!
     
     override func viewDidAppear(_ animated: Bool) {
-        TextField1.becomeFirstResponder()
+        computerPartTextField.becomeFirstResponder()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        textFields = [TextField1, TextField2, TextField3]
+        
+        upgradeSwitch.backgroundColor = UIColor(red: 0.6, green: 0.756, blue: 0.85, alpha: 1);
+        upgradeSwitch.layer.cornerRadius = 16;
+        
+        cloudSupportSwitch.backgroundColor = UIColor(red: 0.6, green: 0.756, blue: 0.85, alpha: 1);
+        cloudSupportSwitch.layer.cornerRadius = 16;
+        
+        listOfBrandTypes = [seagateTypes, toshibaTypes, samsungTypes]
+        textFields = [computerPartTextField, budgetTextField, TextField3]
         errorAlert.frame.origin.y = -1 * errorAlert.frame.size.height
+        
+        hiddenViews = [budgetQuestion, upgradeQuestion, placeholderQuestion]
+        for view in hiddenViews {
+            view.frame.origin.y = screenSize.height + view.frame.size.height
+        }
+        computerPartQuestion.frame.origin.y = screenSize.height / 2 - screenSize.height / 6
+        
+        currentQuestion = computerPartQuestion
         // Do any additional setup after loading the view.
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        if(currentQuestion == budgetQuestion && computerPart == "Storage") {
+            return 3
+        }
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if(currentTextField == TextField1) {
-            return picker1Type[row]
+        if(currentTextField == computerPartTextField) {
+            return computerPartList[row]
         } else if(currentTextField == TextField3) {
             return picker2Type[row]
+        } else if(currentTextField == selectedPartTextField) {
+            if(computerPart == "CPU Processor") {
+                return self.processorTypes[row]
+            } else {
+                //for now this is just storage
+                if(component == 0) {
+                    return storageBrands[row]
+                } else if(component == 1) {
+                    if(currentStorageBrand == "Seagate") {return seagateTypes[row]}
+                    else if(currentStorageBrand == "Toshiba") {return toshibaTypes[row]}
+                    else {return samsungTypes[row]}
+                } else {
+                    return storageSize[row]
+                }
+            }
         }
         return ""
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if(currentTextField == TextField1) {
-            return picker1Type.count
+        if(currentTextField == computerPartTextField) {
+            return computerPartList.count
         } else if(currentTextField == TextField3) {
             return picker2Type.count
+        } else if(currentTextField == selectedPartTextField) {
+            if(computerPart == "CPU Processor") {
+                return 4
+            } else {
+                //currently just for storage
+                if(component == 0) {
+                    return 3
+                } else if(component == 1) {
+                    if(currentStorageBrand == "Seagate") {return seagateTypes.count}
+                    else if(currentStorageBrand == "Toshiba") {return toshibaTypes.count}
+                    else {return samsungTypes.count}
+                } else {
+                    return storageSize.count
+                }
+            }
         }
         return 0
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if(currentTextField == TextField1) {
-            TextField1.text = picker1Type[row]
-            if(TextField1.text == "Storage") {
+        if(currentTextField == computerPartTextField) {
+            computerPartTextField.text = computerPartList[row]
+            if(computerPartTextField.text == "Storage") {
                 CloudSupport.isHidden = false
             } else {
                 CloudSupport.isHidden = true
             }
+            computerPart = computerPartTextField.text!
         } else if(currentTextField == TextField3) {
             TextField3.text = picker2Type[row]
+        } else if(currentTextField == selectedPartTextField) {
+            if(computerPart == "CPU Processor") {
+                selectedPartTextField.text = self.processorTypes[row]
+            } else {
+                //for now, this is if storage
+                if(component == 0) {
+                    currentStorageBrand = storageBrands[row]
+                    pickerView.reloadComponent(1)
+                    selectedPartTextField.text = self.currentStorageBrand + " " + self.listOfBrandTypes[pickerView.selectedRow(inComponent: 0)][pickerView.selectedRow(inComponent: 1)] + " " + storageSize[pickerView.selectedRow(inComponent: 2)]
+                } else if(component == 1) {
+                    selectedPartTextField.text = self.currentStorageBrand + " " + self.listOfBrandTypes[pickerView.selectedRow(inComponent: 0)][row] + " " + storageSize[pickerView.selectedRow(inComponent: 2)]
+                }
+                else {
+                    selectedPartTextField.text = self.currentStorageBrand + " " + self.listOfBrandTypes[pickerView.selectedRow(inComponent: 0)][pickerView.selectedRow(inComponent: 1)] + " " + storageSize[pickerView.selectedRow(inComponent: 2)]
+                }
+            }
         }
         if(currentTextField.tag < textFields.count - 1) {
-            textFields[currentTextField.tag + 1].becomeFirstResponder()
+            //textFields[currentTextField.tag + 1].becomeFirstResponder()
         } else {
-            self.view.endEditing(true)
+            //self.view.endEditing(true)
         }
     }
     
@@ -90,23 +187,53 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     
     @IBAction func submitButtonClicked(_ sender: Any) {
-        for n in 0 ..< textFields.count {
-            if(textFields[n].text == "") {
-                let errorAlert = UIAlertController(title: "Error", message: "You must fill in every text field.", preferredStyle: .alert)
-                
-                errorAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                //self.present(errorAlert, animated: true)
-                UIView.animate(withDuration: 0.4, animations: {
-                    self.errorAlert.frame.origin.y = 0
-                })
-                UIView.animate(withDuration: 0.4, delay: 5, animations: {
-                    self.errorAlert.frame.origin.y = -1 * self.errorAlert.frame.size.height
-                })
-                textFields[n].becomeFirstResponder()
+        if(currentTextField.text != "") {
+            self.view.endEditing(true)
+            var nextQuestion : UIView
+            if(currentQuestion.accessibilityIdentifier == "computerpartquestion") {
+                nextQuestion = upgradeQuestion
+            } else if(currentQuestion.accessibilityIdentifier == "upgradequestion") {
+                print("upgrading")
+                if(upgradeSwitch.isOn) {
+                    selectedPartText.text! = "What is your current " + computerPart.lowercased() + "?"
+                    nextQuestion = budgetQuestion
+                    print("budget")
+                } else {
+                    print("bad")
+                    //currently not implemented, delete next
+                    nextQuestion = computerPartQuestion
+                }
             } else {
-                
+                nextQuestion = computerPartQuestion
             }
+            UIView.animate(withDuration: 0.8, animations: {
+                self.currentQuestion.frame.origin.y = 0 - self.currentQuestion.frame.size.height
+                nextQuestion.frame.origin.y = self.screenSize.height / 2 - self.screenSize.height / 6
+            })
+            currentQuestion = nextQuestion
+        } else {
+            print(2)
+            UIView.animate(withDuration: 0.4, animations: {
+                self.errorAlert.frame.origin.y = 0
+            })
+            UIView.animate(withDuration: 0.4, delay: 5, animations: {
+                self.errorAlert.frame.origin.y = -1 * self.errorAlert.frame.size.height
+            })
+            currentTextField.becomeFirstResponder()
         }
     }
+    //        for n in 0 ..< textFields.count {
+    //            if(textFields[n].text == "") {
+    //                UIView.animate(withDuration: 0.4, animations: {
+    //                    self.errorAlert.frame.origin.y = 0
+    //                })
+    //                UIView.animate(withDuration: 0.4, delay: 5, animations: {
+    //                    self.errorAlert.frame.origin.y = -1 * self.errorAlert.frame.size.height
+    //                })
+    //                textFields[n].becomeFirstResponder()
+    //            } else {
+    //
+    //            }
+    //        }
 }
 
